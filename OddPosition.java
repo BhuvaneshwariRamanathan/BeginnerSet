8fa4766f36191147bc21ec6c2e1d1c8c364b04c7
BhuvaneshwariRamanathan <ramansenthil3@gmail.com>
2016-12-08 18:36:55 +0530

    Create OddPosition.java

---
+++ OddPosition.java
@@ -0,0 +1,18 @@
+import java.util.scanner;
+public class OddPosition{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the string to be reversed");
+String a=s.nextLine();
+String ans="";
+for(int i=0;i<a.length();i++){
+if(i%2==0){
+ans+=a.charAt(i);
+}
+else{
+ans+=a.charAt(a.length()-i-1);
+}
+}
+System.out.println(ans);
+}
+}
